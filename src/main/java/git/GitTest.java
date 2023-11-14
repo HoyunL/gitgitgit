7bff8fe259dbde08ee539f25fc5db1fba81f1e11
(leehoyun) <(dlghdbs7@naver.com)>
2023-11-14 23:57:44 +0900

    addddddd

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -7,6 +7,11 @@ public class GitTest {
         System.out.println("2");
         System.out.println("3");
         System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");
+        System.out.println("8");
+
 
         for (int i = 0; i <5; i++) {
             System.out.println(i);
