8a75359569e21a6f7aa0b70cb702787807f7a2ca
(leehoyun) <(dlghdbs7@naver.com)>
2023-11-14 23:59:48 +0900

    gitTwo

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("2");
         System.out.println("3");
         System.out.println("4");
+        System.out.println("2222222");
 
         for (int i = 0; i <5; i++) {
             System.out.println(i);
